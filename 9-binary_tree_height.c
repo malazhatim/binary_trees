963795fb610678b98fffa9dd5925dc182423a289
malazhatim <malaz.hatim@gmail.com>
2024-02-28 15:42:36 -0800

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,3 +14,4 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((l > h_right) ? l : h_right);
+}
