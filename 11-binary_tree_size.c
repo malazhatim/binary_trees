da7e12c4b71d4f543aad7e7394f79f58a2e86c35
malazhatim <malaz.hatim@gmail.com>
2024-02-28 16:08:52 -0800

    11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,7 +11,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-		size += 1;
+
 		size += binary_tree_size(tree->left);
 		size += binary_tree_size(tree->right);
 
